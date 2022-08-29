f475bcacac35f2a6c62e42d86e2ddf7f3bc99082
Leandro Cunha <leandrocunha016@gmail.com>
2022-08-29 11:21:27 -0300

    Pokemons list in home page

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from '../../services/poke-api.service';
 
 @Component({
   selector: 'poke-list',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeListComponent implements OnInit {
 
-  constructor() { }
+  private setAllPokemons: any;
+  public getAllPokemons: any;
+  public apiError: boolean = false;
+
+  constructor(
+    private pokeApiService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+      },
+      _error => {
+        this.apiError = true;
+      }
+    )
   }
-
+  public getSearch(value: string) {
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+    this.getAllPokemons = filter;
+  }
 }
